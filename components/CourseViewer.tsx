911cb70c2b06201d6f600bd56f80bc75ccb4aac6
piotrtwarog1983-alt <piotr.twarog1983@gmail.com>
2025-12-20 12:58:28 +0100

    Page 48 DE: lower text by 15% (paddingTop calc(2rem + 15%))

--- components/CourseViewer.tsx
+++ components/CourseViewer.tsx
@@ -1211,9 +1211,10 @@ useEffect(() => {
                   <div 
                     className="relative w-full h-full flex justify-center p-8 bg-white overflow-y-auto"
                     style={{
-                      alignItems: (currentPage.pageNumber === 10 && language === 'DE') || (currentPage.pageNumber === 44 && language === 'DE' && isMobile) ? 'flex-start' : 'center',
+                      alignItems: (currentPage.pageNumber === 10 && language === 'DE') || (currentPage.pageNumber === 44 && language === 'DE' && isMobile) || (currentPage.pageNumber === 48 && language === 'DE') ? 'flex-start' : 'center',
                       paddingTop: currentPage.pageNumber === 10 && language === 'DE' ? '6rem' 
                         : currentPage.pageNumber === 44 && language === 'DE' && isMobile ? 'calc(2rem + 25%)' 
+                        : currentPage.pageNumber === 48 && language === 'DE' ? 'calc(2rem + 15%)'
                         : '2rem'
                     }}
                   >
