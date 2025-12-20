4bd5bd1920e03897499e40c3835cc66602fca1c5
piotrtwarog1983-alt <piotr.twarog1983@gmail.com>
2025-12-20 08:45:24 +0100

    Fix translation syntax for camera button

--- components/CourseViewer.tsx
+++ components/CourseViewer.tsx
@@ -1201,7 +1201,7 @@ useEffect(() => {
                                 d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" 
                               />
                             </svg>
-                            {t('course.openCamera')}
+                            {t.course.openCamera}
                           </button>
                         )}
                       </div>
