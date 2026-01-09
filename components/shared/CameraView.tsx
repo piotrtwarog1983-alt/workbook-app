a9b3c3d9a27f9cdda8f3268e046fb0942749d75a
piotrtwarog1983-alt <piotr.twarog1983@gmail.com>
2026-01-09 04:40:47 +0100

    Move camera close button 10% more to the right

--- components/shared/CameraView.tsx
+++ components/shared/CameraView.tsx
@@ -730,7 +730,7 @@ export function CameraView({
             className="absolute z-20 w-10 h-10 flex items-center justify-center text-white text-2xl bg-black/50 rounded-full"
             style={{ 
               top: '80px', // Obniżony względem wskaźnika kąta (który jest na top-16 = 64px)
-              right: '15vw' // Przesunięty bardziej w prawo
+              right: '5vw' // Przesunięty o 10% bardziej w prawo (z 15vw na 5vw)
             }}
           >
             ✕
